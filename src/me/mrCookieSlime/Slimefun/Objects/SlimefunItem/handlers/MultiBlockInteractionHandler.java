098222e57b453461b5708627f441a072d1655b6c
sarhatabaot <sarhatabaot1@gmail.com>
2019-05-16 10:55:14 +0300

    Implement MachineID

--- src/me/mrCookieSlime/Slimefun/Objects/SlimefunItem/handlers/MultiBlockInteractionHandler.java
+++ src/me/mrCookieSlime/Slimefun/Objects/SlimefunItem/handlers/MultiBlockInteractionHandler.java
@@ -5,8 +5,7 @@ import me.mrCookieSlime.Slimefun.Objects.MultiBlock;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
-public abstract class MultiBlockInteractionHandler extends ItemHandler {
-	public abstract String getMachineID();
+public abstract class MultiBlockInteractionHandler extends ItemHandler implements MachineID{
 	public abstract boolean onInteract(Player p, MultiBlock mb, Block b);
 	
 	@Override
